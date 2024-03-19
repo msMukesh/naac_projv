13661698a08e9b3c352ecc49505fb31ecec52dff
anjana732 <anjanasingh1257@gmail.com>
2024-03-19 23:08:25 +0530

    Designed Home Page

---
+++ src/components/UniversityHeader.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function UniversityHeader() {
+  return (
+    <>
+        <center>
+        <h2>Pondicherry University</h2>
+        <p>{`(A Central University)`}</p>
+        </center>
+    </>
+  )
+}
+
+export default UniversityHeader
