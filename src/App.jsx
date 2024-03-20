9c4ac218f99ad611de103197ec05570d09296985
msMukesh <msmukesh2001@gmail.com>
2024-03-20 11:35:23 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Auth from "./components/auth";
 import Dashboard from "./components/dashboard";
-import MyHomePage from "./components/MyHomePage";
 import { useUserContext } from "./context/userContext";
-
+import Criterion3 from "./components/criterion3";
 function App() {
   const { user, loading, error } = useUserContext();
 
   return (
-    <>
-      {error && <p className="error">{error}</p>}
-      {loading ? <h2>Loading...</h2> : <> {user ? <Dashboard /> : <Auth />} </>}
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              {error && <p className="error">{error}</p>}
+              {loading ? (
+                <h2>Loading...</h2>
+              ) : (
+                <>
+                  {user ? <Dashboard /> : <Auth />}
+                </>
+              )}
+            </>
+          }
+        />
+        <Route path="/criterion3" element={<Criterion3 />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
