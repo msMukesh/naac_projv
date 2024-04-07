8569198325754f638a316e9b4c20159c405c5f4d
Mukesh <msmukesh2001@gmail.com>
2024-04-07 20:07:07 +0530

    spelling corrected

--- src/components/criterion1.jsx
+++ src/components/criterion1.jsx
@@ -1,8 +1,8 @@
 import React, { useState} from 'react'
 import NavBar from './Navbar'
 
-function criterion1() {
-    const [rowCount, setRowCount] = useState(1);
+const Criterion1 = () => {
+  const [rowCount, setRowCount] = useState(1);
     const [tableData, setTableData] = useState([Array(6).fill('')]);
   
     const handleAddRow = () => {
@@ -165,4 +165,4 @@ function criterion1() {
   )
 }
 
-export default criterion1
+export default Criterion1;
