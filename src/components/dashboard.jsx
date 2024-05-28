e0e9542bbac7c5e00544d1ae8285be68e4cfc119
Mukesh <msmukesh2001@gmail.com>
2024-05-28 15:51:58 +0530

    updated dashboard

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -17,14 +17,21 @@ const Dashboard = () => {
 
   return (
     <div className="dashboardContainer">
-          <NavBar logoutUser={logoutUser} />
+      <NavBar logoutUser={logoutUser} />
+      <div className="dashboard-content">
+        <h1>Dashboard</h1>
+        <h2>Name: {userName}</h2> {/* Display userName state */}
+        <h2>Email: {user.email}</h2>
 
-      <div className="dashboard-container">
+        <div className="criteria-info">
+          <h3>Criterion I: Curricular Aspects</h3>
+          <p>Focuses on the design and development of the curriculum.</p>
 
-        <div className="content">
-          <h1>Dashboard</h1>
-          <h2>Name: {userName}</h2> {/* Display userName state */}
-          <h2>Email: {user.email}</h2>
+          <h3>Criterion II: Teaching-Learning and Evaluation</h3>
+          <p>Evaluates the quality of teaching, learning, and assessment methods.</p>
+
+          <h3>Criterion III: Research, Innovations, and Extension</h3>
+          <p>Promotes research activities, innovation, and extension programs.</p>
         </div>
       </div>
     </div>
